fc1c70623497840903cb1852a35acef79ada1045
Soukac <soukainaaitchrif2004@gmail.com>
2025-04-05 20:24:39 +0000

    Consultation de tous les patients

--- src/main/java/ma/enset/tp2_ormjpahibernatespringdata/Tp2OrmjpaHibernateSpringDataApplication.java
+++ src/main/java/ma/enset/tp2_ormjpahibernatespringdata/Tp2OrmjpaHibernateSpringDataApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.text.SimpleDateFormat;
+import java.util.List;
 
 @SpringBootApplication
 public class Tp2OrmjpaHibernateSpringDataApplication implements CommandLineRunner {
@@ -26,7 +27,11 @@ public class Tp2OrmjpaHibernateSpringDataApplication implements CommandLineRunne
 		patientRepository.save(new JPAPatient(null,"aitchrif1",sdf.parse("2004-02-16"),false,16));
 		patientRepository.save(new JPAPatient(null,"aitchrif2",sdf.parse("2004-02-17"),false,17));
 		patientRepository.save(new JPAPatient(null,"aitchrif3",sdf.parse("2004-02-18"),false,18));
+        List<JPAPatient> patients = patientRepository.findAll();
+        patients.forEach(p->{
+			System.out.println(p.toString());
+				}
 
-
+		);
 	}
 }
